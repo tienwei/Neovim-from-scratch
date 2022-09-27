2fdd861dec7fd669068a33c889964f0f217bae6c
Tien <tienlindev@gmail.com>
2022-09-28 00:00:13 +1000

    fix - treesitter issue

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,12 +83,12 @@ return packer.startup(function(use)
 
 	-- Telescope
 	use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
-
+  use({"nvim-treesitter/nvim-treesitter"})
 	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
-	})
+	-- use({
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	commit = "518e27589c0463af15463c9d675c65e464efc2fe",
+	-- })
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
