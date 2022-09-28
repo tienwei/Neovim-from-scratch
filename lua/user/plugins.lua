7b896a4288f2c453bd5b3ef6dc6db7745a50551d
tlin3 <tlin3@atlassian.com>
2022-09-28 22:14:32 +1000

    chore - clean up treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,12 +83,9 @@ return packer.startup(function(use)
 
 	-- Telescope
 	use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
-  use({"nvim-treesitter/nvim-treesitter"})
+  
 	-- Treesitter
-	-- use({
-	-- 	"nvim-treesitter/nvim-treesitter",
-	-- 	commit = "518e27589c0463af15463c9d675c65e464efc2fe",
-	-- })
+  use({"nvim-treesitter/nvim-treesitter"})
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
@@ -99,3 +96,5 @@ return packer.startup(function(use)
 		require("packer").sync()
 	end
 end)
+
+
